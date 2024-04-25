145296b43863be64a4be32ed625432949a7c596b
banjoSolomon <ayomidebanjo68@gmail.com>
2024-04-25 10:56:41 +0100

    update

--- objects.js
+++ objects.js
@@ -8,3 +8,19 @@ let dog = Object.create(animal)
 dog.type = "Dog";
 dog.color = "brown"
 console.log(dog)
+
+let goat = {}
+Object.setPrototypeOf(goat, animal)
+goat.type = "Goat"
+console.log(goat)
+
+
+let vehicle = {wheels : 4};
+let car = {
+    seats: 5,
+    _proto_: vehicle,
+    wheels: 6,
+}
+console.log('vehicle:', vehicle, vehicle._proto_);
+console.log('car:', car, car._proto_);
+console.log('car wheels:', car.wheels);
